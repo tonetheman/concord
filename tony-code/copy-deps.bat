43f41964f83a497e75fb4e71281060a93a797c64
tonetheman <tonetheman@gmail.com>
2013-09-16 23:06:08 -0500

    my example0
    example0 with no external deps, updated font-awesome to serve from
    single directory

---
+++ tony-code/copy-deps.bat
@@ -0,0 +1,8 @@
+rem
+copy ..\concord.css .
+copy ..\concord.js .
+copy ..\concordUtils.js .
+copy ..\libraries\bootstrap.css
+copy ..\libraries\bootstrap.min.js
+rem copy font-awesome\css\font-awesome.css .
+copy font-awesome\font\* .
